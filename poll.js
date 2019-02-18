71c16210ac2d53d90e0e828d6a108b53dc81535c
Donna <skies09@hotmail.com>
2019-02-18 11:47:22 +0000

    initial commit

---
+++ poll.js
@@ -0,0 +1,198 @@
+var gBP = 0;
+var pS = 0;
+var kCC = 0;
+var nEP = 0;
+var bB = 0;
+var cP = 0;
+var sS = 0;
+var iC = 0;
+var aC = 0;
+var bR = 0;
+var hT = 0;
+var nOS = 0;
+var pE = 0;
+var dB = 0;
+var dL = 0;
+var mV = 0;
+var nYG = 0;
+var aF = 0;
+var dC = 0;
+var jJ = 0;
+var mD = 0;
+var cBen = 0;
+var oR = 0;
+var tBB = 0;
+var lAR = 0;
+var cBear = 0;
+var cBrown = 0;
+var sDC = 0;
+var sF = 0;
+var nYJ = 0;
+var wR = 0;
+var tT = 0;
+
+function refreshResults() {
+    var results = document.getElementById('results');
+    results.innerHTML = 'total: ' + (gBP + pS + kCC + nEP + bB + cP + sS + aC + bR + hT + nOS + pE + dB + dL + mV + nYG + aF + dC + jJ + mD + cBen + oR + tBB + lAR + cBear + cBrown + sDC + sF + nYJ + wR + tT);
+    results.innerHTML += '<br/>Green Bay Packers:' + gBP;
+    results.innerHTML += '<br/>Pittsburgh Steelers:' + pS;
+    results.innerHTML += '<br/>Kansas City Chiefs:' + kCC;
+    results.innerHTML += '<br/>New England Patriots:' + nEP;
+    results.innerHTML += '<br/>Buffalo Bills:' + bB;
+    results.innerHTML += '<br/>Carolina Panthers:' + cP;
+    results.innerHTML += '<br/>Seattle Seahawks:' + sS;
+    results.innerHTML += '<br/>Indianapolis Colts:' + iC;
+    results.innerHTML += '<br/>Arizona Cardinals:' + aC;
+    results.innerHTML += '<br/>Baltimore Ravens:' + bR;
+    results.innerHTML += '<br/>Houston Texans:' + hT;
+    results.innerHTML += '<br/>New Orleans Saints:' + nOS;
+    results.innerHTML += '<br/>Philadelphia Eagles:' + pE;
+    results.innerHTML += '<br/>Denver Broncos:' + dB;
+    results.innerHTML += '<br/>Detroit Lions:' + dL;
+    results.innerHTML += '<br/>Minnesota Vikings:' + mV;
+    results.innerHTML += '<br/>New York Giants:' + nYG;
+    results.innerHTML += '<br/>Atlanta Falcons:' + aF;
+    results.innerHTML += '<br/>Dallas Cowboys:' + dC;
+    results.innerHTML += '<br/>Jacksonville Jaguars:' + jJ;
+    results.innerHTML += '<br/>Miami Dolphins:' + mD;
+    results.innerHTML += '<br/>Cincinnati Bengals:' + cBen;
+    results.innerHTML += '<br/>Oakland Raiders:' + oR;
+    results.innerHTML += '<br/>Tampa Bay Buccaneers:' + tBB;
+    results.innerHTML += '<br/>Los Angeles Rams:' + lAR;
+    results.innerHTML += '<br/>Chicago Bears:' + cBear;
+    results.innerHTML += '<br/>Cleveland Browns:' + cBrown;
+    results.innerHTML += '<br/>San Diego Chargers:' + sDC;
+    results.innerHTML += '<br/>San Francisco 49ers:' + sF;
+    results.innerHTML += '<br/>New York Jets:' + nYJ;
+    results.innerHTML += '<br/>Washington Redskins:' + wR;
+    results.innerHTML += '<br/>Tennessee Titans:' + tT;
+}
+
+document.getElementById('gBP').addEventListener('click', function() {
+    gBP++;
+    refreshResults();
+});
+document.getElementById('pS').addEventListener('click', function() {
+    pS++;
+    refreshResults();
+});
+document.getElementById('kCC').addEventListener('click', function() {
+    kCC++;
+    refreshResults();
+});
+document.getElementById('nEP').addEventListener('click', function() {
+    nEP++;
+    refreshResults();
+});
+document.getElementById('bB').addEventListener('click', function() {
+    bB++;
+    refreshResults();
+});
+document.getElementById('cP').addEventListener('click', function() {
+    cP++;
+    refreshResults();
+});
+document.getElementById('sS').addEventListener('click', function() {
+    sS++;
+    refreshResults();
+});
+document.getElementById('iC').addEventListener('click', function() {
+    iC++;
+    refreshResults();
+});
+document.getElementById('aC').addEventListener('click', function() {
+    aC++;
+    refreshResults();
+});
+document.getElementById('bR').addEventListener('click', function() {
+    bR++;
+    refreshResults();
+});
+document.getElementById('hT').addEventListener('click', function() {
+    hT++;
+    refreshResults();
+});
+document.getElementById('nOS').addEventListener('click', function() {
+    nOS++;
+    refreshResults();
+});
+document.getElementById('pE').addEventListener('click', function() {
+    pE++;
+    refreshResults();
+});
+document.getElementById('dB').addEventListener('click', function() {
+    dB++;
+    refreshResults();
+});
+document.getElementById('dL').addEventListener('click', function() {
+    dL++;
+    refreshResults();
+});
+document.getElementById('mV').addEventListener('click', function() {
+    mV++;
+    refreshResults();
+});
+document.getElementById('nYG').addEventListener('click', function() {
+    nYG++;
+    refreshResults();
+});
+document.getElementById('aF').addEventListener('click', function() {
+    aF++;
+    refreshResults();
+});
+document.getElementById('dC').addEventListener('click', function() {
+    dC++;
+    refreshResults();
+});
+document.getElementById('jJ').addEventListener('click', function() {
+    jJ++;
+    refreshResults();
+});
+document.getElementById('mD').addEventListener('click', function() {
+    mD++;
+    refreshResults();
+});
+document.getElementById('cBen').addEventListener('click', function() {
+    cBen++;
+    refreshResults();
+});
+document.getElementById('oR').addEventListener('click', function() {
+    oR++;
+    refreshResults();
+});
+document.getElementById('tBB').addEventListener('click', function() {
+    tBB++;
+    refreshResults();
+});
+document.getElementById('lAR').addEventListener('click', function() {
+    lAR++;
+    refreshResults();
+});
+document.getElementById('cBear').addEventListener('click', function() {
+    cBear++;
+    refreshResults();
+});
+document.getElementById('cBrown').addEventListener('click', function() {
+    cBrown++;
+    refreshResults();
+});
+document.getElementById('sDC').addEventListener('click', function() {
+    sDC++;
+    refreshResults();
+});
+document.getElementById('sF').addEventListener('click', function() {
+    sF++;
+    refreshResults();
+});
+document.getElementById('nYJ').addEventListener('click', function() {
+    nYJ++;
+    refreshResults();
+});
+document.getElementById('wR').addEventListener('click', function() {
+    wR++;
+    refreshResults();
+});
+document.getElementById('tT').addEventListener('click', function() {
+    tT++;
+    refreshResults();
+});
